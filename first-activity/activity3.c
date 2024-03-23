e59b90457cacbb3e64fa8275667be568c31d5030
guiruyz <guiruyz@gmail.com>
2024-03-23 10:18:32 -0300

    Fixed some contents

--- first-activity/activity3.c
+++ first-activity/activity3.c
@@ -5,10 +5,10 @@ int main() {
     int num;
     printf("Type an entire number: ");
     scanf("%d", &num);
-    if (numero % 2 == 0) {
-        printf("%d It's divisive by 2\n", numero);
+    if (num % 2 == 0) {
+        printf("%d It's divisive by 2\n", num);
     } else {
-        printf("%d It's not divisive by 2\n", numero);
+        printf("%d It's not divisive by 2\n", num);
         }
     return 0;
 }
